0b3f1a7e648f4ee4b9e001e388241641f53af7e3
Chris Erlendson <cke2106@columbia.edu>
2013-05-12 16:43:45 -0400

    putting after_initialize in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User
   include Mongoid::Document
   field :name, type: String
   field :email, type: String
+  field :role, type: String
 
   has_and_belongs_to_many :courses
+
+
+def after_initialize
+  if new_record?
+  	role = "student"
+  end
+end
+
 end
